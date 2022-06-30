ad5513093c336e028a0c5eba1ddc102d22ff38a0
kubicakacper <kubicakacper@gmail.com>
2022-06-30 14:34:43 +0200

    update

--- some_package/some_class.py
+++ some_package/some_class.py
@@ -2,4 +2,8 @@ class SomeClass:
 
     @classmethod
     def print_sth(cls):
-        print('something')
+        with open('./txt_files/input.txt', 'r') as input_stream:
+            print('inside the function')
+            content = input_stream.read()
+            with open('./txt_files/output.txt') as output_stream:
+                output_stream.write(content)
